1d73689723eda32a2fc60b885b6f414e94fc4551
37trillion <96641975+37trillion@users.noreply.github.com>
2023-05-15 02:40:52 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,54 +1,37 @@
-// JavaScript code for Diamond Drillers BCH website
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us</title>
+	<script>
+		function validateForm() {
+			var name = document.forms["contactForm"]["name"].value;
+			var email = document.forms["contactForm"]["email"].value;
+			var message = document.forms["contactForm"]["message"].value;
+			if (name == "" || email == "" || message == "") {
+				alert("Please fill in all fields");
+				return false;
+			}
+			else {
+				alert("Thank you for your message! We will get back to you soon.");
+				return true;
+			}
+		}
+	</script>
+</head>
+<body>
+	<h1>Contact Us</h1>
+	<form name="contactForm" onsubmit="return validateForm()" method="post" action="mailto:ericmccormack92@gmail.com">
+		<label for="name">Name:</label>
+		<input type="text" name="name" required><br>
 
-// Function to display current time
-function displayTime() {
-  let currentDate = new Date();
-  let currentHour = currentDate.getHours();
-  let currentMinute = currentDate.getMinutes();
-  let currentSecond = currentDate.getSeconds();
-  let meridian = "AM";
-  if (currentHour > 12) {
-    currentHour = currentHour - 12;
-    meridian = "PM";
-  }
-  if (currentHour === 0) {
-    currentHour = 12;
-  }
-  if (currentMinute < 10) {
-    currentMinute = "0" + currentMinute;
-  }
-  if (currentSecond < 10) {
-    currentSecond = "0" + currentSecond;
-  }
-  let currentTime = currentHour + ":" + currentMinute + ":" + currentSecond + " " + meridian;
-  document.getElementById("current-time").innerHTML = currentTime;
-}
+		<label for="email">Email:</label>
+		<input type="email" name="email" required><br>
 
-// Function to update BCH price from API
-function updateBCHPrice() {
-  fetch("https://api.coinbase.com/v2/prices/BCH-USD/spot")
-    .then(response => response.json())
-    .then(data => {
-      let bchPrice = data.data.amount;
-      document.getElementById("bch-price").innerHTML = bchPrice;
-    })
-    .catch(error => console.error(error));
-}
+		<label for="message">Message:</label><br>
+		<textarea name="message" required></textarea><br>
 
-// Function to connect to the BCH stratum
-function connectToStratum() {
-  let stratum = "stratum+tcp://192.168.0.58:8333";
-  console.log("Connecting to BCH stratum: " + stratum);
-  // Code to connect to stratum goes here
-}
-
-// Event listener for page load
-window.addEventListener("load", function() {
-  // Display current time
-  displayTime();
-  setInterval(displayTime, 1000);
-
-  // Update BCH price
-  updateBCHPrice();
-  setInterval(updateBCHPrice, 60000);
-});
+		<input type="submit" value="Submit">
+		<input type="reset" value="Clear">
+	</form>
+</body>
+</html>
